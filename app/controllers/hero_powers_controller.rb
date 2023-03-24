197c89f0c15de454886af1f2058143e6bc55110c
abdishukri <abdishukrimohamed105@gmail.com>
2023-03-24 15:19:36 +0300

    completed heropowers controller actions

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -5,22 +5,23 @@ class HeroPowersController < ApplicationController
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   
       def index
-          restaurantspizza = RestaurantPizza.all
-          render json: restaurantspizza
+          hero_powers = HeroPower.all
+          render json: hero_powers
       end
   
       def create
-          restaurant_pizza =  RestaurantPizza.create!(restaurantpizza_params)
-          render json: restaurant_pizza.pizza.to_json(except: [:created_at, :updated_at]), status: :created
+          hero_power =  HeroPower.create!(hero_power_params)
+          render json: hero_power.to_json(except: [:created_at, :updated_at]), status: :created
       end
   
   private
   
-    def restaurantpizza_params
-      params.permit(:price, :pizza_id, :restaurant_id)
+    def hero_power_params
+      params.permit(:strength, :power_id, :hero_id)
     end
   
     def render_unprocessable_entity_response(invalid)
       render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
     end
+    
 end
